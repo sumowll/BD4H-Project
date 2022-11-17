797846e60ede5f8a7aab6673e58e6aabf0cfe3f6
stqchang <54524850+stqchang@users.noreply.github.com>
2022-11-16 21:19:22 -0500

    reduce epoch

--- src/main_pretrain.py
+++ src/main_pretrain.py
@@ -23,7 +23,8 @@ import loader
 
 
 def make_idx_data_term(dataset, args):
-    data = []  # [(node_id, [(node_id1, ppmi?count?), (node_id2, ppmi?count?), ...]), (), ...]
+    # [(node_id, [(node_id1, ppmi?count?), (node_id2, ppmi?count?), ...]), (), ...]
+    data = []
     for cur_sample in dataset:
         cur_dict = {}
         cur_dict['id'] = cur_sample[0]
@@ -38,7 +39,8 @@ def make_idx_data_term(dataset, args):
         cur_dict['ngram_ids'] = ngram_ids
 
         word_list = args.term_strings[cur_sample[0]].split()
-        cur_dict['word_ids'] = [args.word_to_id[w if w in args.word_to_id else '<UNK>'] for w in word_list]
+        cur_dict['word_ids'] = [
+            args.word_to_id[w if w in args.word_to_id else '<UNK>'] for w in word_list]
         cur_dict['word_len'] = len(word_list)
 
         # one-hot labels
@@ -54,7 +56,8 @@ def make_idx_data_term(dataset, args):
 
 
 def make_idx_term_ns(dataset, args):
-    data = []  # [(node_id, [(node_id1, ppmi?count?), (node_id2, ppmi?count?), ...]), (), ...]
+    # [(node_id, [(node_id1, ppmi?count?), (node_id2, ppmi?count?), ...]), (), ...]
+    data = []
     for cur_sample in dataset:
         cur_dict = {}
         cur_dict['id'] = cur_sample[0]
@@ -69,7 +72,8 @@ def make_idx_term_ns(dataset, args):
         cur_dict['ngram_ids'] = ngram_ids
 
         word_list = args.term_strings[cur_sample[0]].split()
-        cur_dict['word_ids'] = [args.word_to_id[w if w in args.word_to_id else '<UNK>'] for w in word_list]
+        cur_dict['word_ids'] = [
+            args.word_to_id[w if w in args.word_to_id else '<UNK>'] for w in word_list]
         cur_dict['word_len'] = len(word_list)
 
         # context terms
@@ -132,14 +136,19 @@ def main():
     def str2bool(string):
         return string.lower() in ['yes', 'true', 't', 1]
 
-    parser = argparse.ArgumentParser(description='process user given parameters')
+    parser = argparse.ArgumentParser(
+        description='process user given parameters')
     parser.register('type', 'bool', str2bool)
     # path path path
-    parser.add_argument('--embed_filename', type=str, default='../data/embeddings/glove.6B.100d.txt')
-    parser.add_argument('--ngram_embed_path', type=str, default='../data/embeddings/charNgram.txt')
+    parser.add_argument('--embed_filename', type=str,
+                        default='../data/embeddings/glove.6B.100d.txt')
+    parser.add_argument('--ngram_embed_path', type=str,
+                        default='../data/embeddings/charNgram.txt')
 
-    parser.add_argument('-p', "--per", type=str, default='Bin', help='Pat or Bin')
-    parser.add_argument('-d', '--days', type=str, default='1', help='1 7 30 90 180 365 all')
+    parser.add_argument('-p', "--per", type=str,
+                        default='Bin', help='Pat or Bin')
+    parser.add_argument('-d', '--days', type=str,
+                        default='1', help='1 7 30 90 180 365 all')
 
     parser.add_argument('--ngram_embed_dim', type=int, default=100)
     parser.add_argument('--n_grams', type=str, default='2, 3, 4')
@@ -147,18 +156,28 @@ def main():
     parser.add_argument('--word_hidden_dim', type=int, default=100)
     parser.add_argument('--word_embed_dim', type=int, default=100)
 
-    parser.add_argument("--num_epochs", type=int, default=10000, help="number of epochs for training")
-    parser.add_argument("--batch_size", type=int, default=512, help="batch size")
+    #parser.add_argument("--num_epochs", type=int, default=10000, help="number of epochs for training")
+    parser.add_argument("--num_epochs", type=int, default=5,
+                        help="number of epochs for training")
+    parser.add_argument("--batch_size", type=int,
+                        default=512, help="batch size")
     parser.add_argument('--random_seed', type=int, default=43)
-    parser.add_argument("--dropout", type=float, default=0.5, help="size of testing set")
-    parser.add_argument("--log_interval", type=int, default=100, help='step interval for log')
-    parser.add_argument("--test_interval", type=int, default=1, help='epoch interval for testing')
-    parser.add_argument("--early_stop_epochs", type=int, default=1000, help='epoch interval for early stopping')
+    parser.add_argument("--dropout", type=float,
+                        default=0.5, help="size of testing set")
+    parser.add_argument("--log_interval", type=int,
+                        default=100, help='step interval for log')
+    parser.add_argument("--test_interval", type=int,
+                        default=1, help='epoch interval for testing')
+    parser.add_argument("--early_stop_epochs", type=int,
+                        default=1000, help='epoch interval for early stopping')
     parser.add_argument('--learning_rate', type=float, default=0.0001)
 
-    parser.add_argument("--save_best", type='bool', default=True, help='save model in the best epoch or not')
-    parser.add_argument("--save_dir", type=str, default='./saved_models/saved_pretrained')
-    parser.add_argument("--save_interval", type=int, default=1, help='intervals for saving models')
+    parser.add_argument("--save_best", type='bool', default=True,
+                        help='save model in the best epoch or not')
+    parser.add_argument("--save_dir", type=str,
+                        default='./saved_models/saved_pretrained')
+    parser.add_argument("--save_interval", type=int,
+                        default=1, help='intervals for saving models')
 
     parser.add_argument('--neg_sampling', type='bool', default=False)
     parser.add_argument('--num_negs', type=int, default=5)
@@ -172,14 +191,17 @@ def main():
     # global variables
     args.cuda = torch.cuda.is_available()
 
-    args.term_strings = pickle.load(open('../data/mappings/term_string_mapping.pkl', 'rb'))
-    dataset = pickle.load(open('../data/sym_data/sub_neighbors_dict_ppmi_per' + args.per + '_' + args.days + '.pkl', 'rb'))
+    args.term_strings = pickle.load(
+        open('../data/mappings/term_string_mapping.pkl', 'rb'))
+    dataset = pickle.load(open(
+        '../data/sym_data/sub_neighbors_dict_ppmi_per' + args.per + '_' + args.days + '.pkl', 'rb'))
 
     # prepare context labels
     context_terms = list(dataset.keys())
     print('Total number of candidates: ', len(context_terms))
 
-    reuse_stored_path = './saved_models/{0}_{1}_pretrain_model_dict.pkl'.format(args.per, args.days)
+    reuse_stored_path = './saved_models/{0}_{1}_pretrain_model_dict.pkl'.format(
+        args.per, args.days)
     if os.path.exists(reuse_stored_path):
         model_dict = pickle.load(open(reuse_stored_path, 'rb'))
         # output
@@ -208,13 +230,15 @@ def main():
 
         args.n_grams = [int(x) for x in args.n_grams.split(',')]
         list_words = list(set([x for x in args.term_strings.values()]))
-        ngram_vocab, ngram_to_id, id_to_gram, pre_train_ngram = loader.g2v_mapping_pretrain(list_words, args)
+        ngram_vocab, ngram_to_id, id_to_gram, pre_train_ngram = loader.g2v_mapping_pretrain(
+            list_words, args)
         args.pre_train_ngram = pre_train_ngram
         args.ngram_to_id = ngram_to_id
         args.ngram_vocab_size = len(ngram_to_id)
 
         list_phrases = args.term_strings.values()
-        word_vocab, word_to_id, id_to_word, pre_train_words = loader.w2v_mapping_pretrain(list_phrases, args)
+        word_vocab, word_to_id, id_to_word, pre_train_words = loader.w2v_mapping_pretrain(
+            list_phrases, args)
         args.pre_train_words = pre_train_words
         args.word_to_id = word_to_id
         args.word_vocab_size = len(word_to_id)
@@ -257,7 +281,8 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)    
     '''
     if args.neg_sampling:
-        degree_list = pickle.load(open('../data/sym_data/degree_list_perBin_1.pkl', 'rb'))
+        degree_list = pickle.load(
+            open('../data/sym_data/degree_list_perBin_1.pkl', 'rb'))
         weights = np.zeros(args.node_vocab_size)
         for y in degree_list:
             if y[0] in args.node_to_id:
@@ -288,7 +313,8 @@ def main():
             train_loss = []
             np.random.shuffle(train_idx)
             for i in range(num_batches):
-                train_batch = train_idx[i * args.batch_size: (i + 1) * args.batch_size]
+                train_batch = train_idx[i *
+                                        args.batch_size: (i + 1) * args.batch_size]
                 if i == num_batches - 1:
                     train_batch = train_idx[i * args.batch_size::]
 
@@ -308,9 +334,11 @@ def main():
                 steps += 1
 
                 if steps % args.log_interval == 0:
-                    print('Epoch {0} step {1} - Train loss\u2193:{2:.10}'.format(epoch, steps, np.mean(train_loss)))
+                    print(
+                        'Epoch {0} step {1} - Train loss\u2193:{2:.10}'.format(epoch, steps, np.mean(train_loss)))
 
-            print('Epoch {0} - Train loss\u2193:{1:.10}'.format(epoch, np.mean(train_loss)))
+            print(
+                'Epoch {0} - Train loss\u2193:{1:.10}'.format(epoch, np.mean(train_loss)))
 
             # if epoch % args.save_interval == 0:
             # if steps % 10000 == 0:
@@ -346,7 +374,8 @@ def main():
             train_loss = 0.0
             np.random.shuffle(train_idx)
             for i in range(num_batches):
-                train_batch = train_idx[i * args.batch_size: (i + 1) * args.batch_size]
+                train_batch = train_idx[i *
+                                        args.batch_size: (i + 1) * args.batch_size]
                 if i == num_batches - 1:
                     train_batch = train_idx[i * args.batch_size::]
 
@@ -368,7 +397,8 @@ def main():
 
                 steps += 1
 
-            print('Epoch {0} - Train loss\u2193:{1:.10}'.format(epoch, train_loss / len(train)))
+            print(
+                'Epoch {0} - Train loss\u2193:{1:.10}'.format(epoch, train_loss / len(train)))
 
             if epoch % args.save_interval == 0:
                 print(datetime.now().strftime("%m/%d/%Y %X"))
@@ -379,4 +409,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
